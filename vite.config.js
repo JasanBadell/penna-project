a0b25d935dc7f81c5e094c6b3c8ccc0301850907
jasanbadell <jasanbadelldev@gmail.com>
2024-01-12 17:13:08 -0500

    Last Update 12/1/2024

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // base: "/penna-project/",
+  base: "/penna-project/",
+  build: { chunkSizeWarningLimit: 1600 },
 });
